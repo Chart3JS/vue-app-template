d1cc296145fbb2bbf7f1bed9883c698235cabee1
Alexander Plotkin <jprjdev@gmail.com>
2018-08-06 14:55:59 +0300

    fix text

--- src/pages/Dashboard/Dashboard.vue
+++ src/pages/Dashboard/Dashboard.vue
@@ -2,7 +2,7 @@
   <v-container fluid>
     <v-slide-y-transition mode="out-in">
       <v-layout column align-center>
-        AAAAA
+        Dashboard
       </v-layout>
     </v-slide-y-transition>
   </v-container>
